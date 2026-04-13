48dc9adf3525cd99f13a6893dc34f9af2f082ef0
ChinmayiiS <108892858+ChinmayiiS@users.noreply.github.com>
2026-04-13 08:52:37 +0530

    Add training function for XGBClassifier model

---
+++ src/models/train.py
@@ -0,0 +1,10 @@
+from xgboost import XGBClassifier
+
+def train_model(X_train, y_train):
+    model = XGBClassifier(
+        n_estimators=100,
+        max_depth=5,
+        learning_rate=0.1
+    )
+    model.fit(X_train, y_train)
+    return model
